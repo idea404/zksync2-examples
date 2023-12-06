e382abedadfe8af33e3ac5df2244b1f5c6d85ab4
Dennis <10233439+idea404@users.noreply.github.com>
2023-12-06 18:56:53 +0200

    make script 19

--- js/src/19_use_paymaster.ts
+++ js/src/19_use_paymaster.ts
@@ -7,8 +7,8 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const wallet = new Wallet(PRIVATE_KEY, provider, ethProvider);
 
 
-const tokenAddress = "0xCd9BDa1d0FC539043D4C80103bdF4f9cb108931B"; // Crown token which can be minted for free
-const paymasterAddress = "0xd660c2F92d3d0634e5A20f26821C43F1b09298fe"; // Paymaster for Crown token
+const tokenAddress = "0xef16000d668a6c5343a8b07bBEC8682C2AA4FdA4"; // Crown token which can be minted for free
+const paymasterAddress = "0xc08aCd67c48818CaC2599Bfb585B411FFD5a0BF2"; // Paymaster for Crown token
 const token = new ethers.Interface(require('../../solidity/custom_paymaster/token/build/Token.json').abi);
 
 
@@ -19,16 +19,18 @@ During transaction execution, the paymaster receives the ERC20 token from the us
  */
 async function main() {
     console.log(`Paymaster balance before mint: ${await provider.getBalance(paymasterAddress)}`);
-    console.log(`Wallet address before mint: ${await wallet.getBalance()}`);
+    console.log(`Wallet balance before mint: ${await wallet.getBalance()}`);
     console.log(`Paymaster Crown tokens before mint: ${await provider.getBalance(paymasterAddress, 'latest', tokenAddress)}`);
     console.log(`Wallet Crown tokens before mint: ${await wallet.getBalance(tokenAddress)}`);
 
+    console.log(`Minting 3 Crown tokens for wallet...`);
     // Also mint some tokens to user account, so it can offer to pay fee with it
     const mintTx = await wallet.sendTransaction({
         to: tokenAddress,
         data: token.encodeFunctionData('mint', [await wallet.getAddress(), 10]),
     });
     await mintTx.wait();
+    console.log(`Minted 3 Crown tokens for wallet`);
 
     // Transfer some ETH to paymaster, so it can pay fee with ETH
     const faucetTx = await wallet.transfer({
@@ -37,7 +39,7 @@ async function main() {
         amount: ethers.parseEther('0.1')
     });
     await faucetTx.wait();
-
+    console.log(`Transferred 0.1 ETH to paymaster`);
 
     // In order to use paymaster, EIP712 transaction
     // need to be created with configured paymaster parameters
